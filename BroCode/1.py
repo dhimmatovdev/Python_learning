f8fdbd947f031d10cd660b27b79e9e301fba561f
Alexia Tressie <alexiatr@sh-i4.skd.21-school.uz>
2025-02-25 14:43:32 +0500

    bro code

--- BroCode/1.py
+++ BroCode/1.py
@@ -1,3 +1,25 @@
+#strings 
 first_name = 'Bro'
 nick = 'Code'
-print(f"You are {first_name} {nick}")
+
+#integers
+age = 25
+quantity = 3
+num_f_students = 30
+
+#Float
+price = 10.98
+gpa = 4.9
+
+# print(f"The price is ${price}")
+# print(f"GPA is {gpa}")
+
+#boolean
+
+is_student = False
+
+# print(f"Are you a student?: {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
