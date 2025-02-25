927f863ef3344f1ad0a1517dcbbcfd6384aa2798
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-02-25 15:05:20 +0500

    typecasting

---
+++ BroCode/3.py
@@ -0,0 +1,8 @@
+# input() = A function that promts the user to enter data 
+# Return the entered data as a string
+
+name = input("What is yuur name: ")
+age = int(input("How old are you: "))
+age = age+1
+print(f"Hello {name} Sen {age}{type(age)} yoshdasan bir yildan song")
+
