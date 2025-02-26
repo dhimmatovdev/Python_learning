892d092dfb622f47f575a63f43fdb2d1297a979c
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-02-26 11:12:46 +0500

    math arth

---
+++ BroCode/5.py
@@ -0,0 +1,16 @@
+friends = 10
+#friends = friends + 1
+#friends += 1 # equal  
+#friends -=2 
+
+#friends = friends * 3
+#friends *= 3
+
+#friends = friends / 2
+#friends /= 2
+
+#friends = friends ** 2
+#friends **=2
+
+#  pow 
+# max result = max(x, y, z)
