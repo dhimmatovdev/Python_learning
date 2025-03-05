63055dba2205d615459aab2618ee7b75b65a5f11
Dilshod Himmatov <dilshodhimmatov06@gmail.com>
2025-03-05 14:04:22 +0500

    05.03

--- coursera/main0.py
+++ coursera/main0.py
@@ -1,19 +1,21 @@
-message = "What's up, Doc?"
-n = 17
-pi = 3.14159
+# message = "What's up, Doc?"
+# n = 17
+# pi = 3.14159
 
-print(message)
-print(n)
-print(pi)
+# print(message)
+# print(n)
+# print(pi)
 
-day = "Thursday"
-print(day)
-day = "Friday"
-print(day)
-day = 21
-print(day)
+# day = "Thursday"
+# print(day)
+# day = "Friday"
+# print(day)
+# day = 21
+# print(day)
 
-day = "Thursday"
-day = 32.5
-day = 19
-print(day)
+# day = "Thursday"
+# day = 32.5
+# day = 19
+# print(day)
+print(1 + 1 + (2 * 3))
+print(len("hello"))
