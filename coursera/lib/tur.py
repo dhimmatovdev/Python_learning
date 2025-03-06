32b41fb7304f32d3844bc951e3dd4150d4dd9f56
Alexiatr <himmatovdilshod06@gmail.com>
2025-03-06 11:43:37 +0500

    06.03

---
+++ coursera/lib/tur.py
@@ -0,0 +1,19 @@
+import turtle 
+
+pen = turtle.Turtle() 
+pen.forward(100)
+pen.left(90)
+pen.forward(100)
+
+turtle.done()
+# import turtle
+
+# pen = turtle.Turtle()
+# pen.pensize(3)  # Chizish qalinligi
+# pen.pencolor("red")  # Chizish rangi
+
+# pen.forward(100)
+# pen.left(90)
+# pen.forward(100)
+
+# turtle.done()
