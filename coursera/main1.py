99e4d297b531634385356c04108a450ede6c97ff
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-03-08 14:14:25 +0500

    08.03

--- coursera/main1.py
+++ coursera/main1.py
@@ -1,10 +1,17 @@
-a = int(input("Hozirgi soatni kiriting (0-23): "))  # Soatni butun songa o‘giramiz
-x = int(input("Necha soat kutmoqchisiz? "))  # Kutish vaqtini butun songa o‘giramiz
+# a = int(input("Hozirgi soatni kiriting (0-23): "))  # Soatni butun songa o‘giramiz
+# x = int(input("Necha soat kutmoqchisiz? "))  # Kutish vaqtini butun songa o‘giramiz
 
-h = x // 24  # To‘liq kunlar
-s = x % 24  # Qolgan soatlar
+# h = x // 24  # To‘liq kunlar
+# s = x % 24  # Qolgan soatlar
 
-a = (a + s) % 24  # Yangi vaqtni hisoblash
+# a = (a + s) % 24  # Yangi vaqtni hisoblash
 
-print(f"{h} kun va {s} soat o‘tdi")
-print(f"Hozirgi vaqt: {a}:00")
+# print(f"{h} kun va {s} soat o‘tdi")
+# print(f"Hozirgi vaqt: {a}:00")
+str_time = input("What time is it now? ")
+str_wait_time = input("What is the number of hours to wait? ")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
