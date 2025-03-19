0a038fa512fb66aa2868661976326539a0ae71da
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-03-19 13:28:26 +0500

    19.03

---
+++ easyleet/383.py
@@ -0,0 +1,6 @@
+# Counter: 
+# Counter — bu Python dasturida elementlar sonini sanash uchun 
+# ishlatiladigan maxsus klass (collections.Counter).
+# U list, string, tuple yoki boshqa iteratsion obyektlardagi 
+# takrorlanuvchi elementlarni hisoblab, natijani lug‘at (dictionary) 
+# shaklida qaytaradi.
