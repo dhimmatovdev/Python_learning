792ab17cf55ec5fb3f2fd3fcfbb38f06170e5fc5
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-03-19 13:34:32 +0500

    19.03

--- easyleet/383.py
+++ easyleet/383.py
@@ -4,3 +4,23 @@
 # U list, string, tuple yoki boshqa iteratsion obyektlardagi 
 # takrorlanuvchi elementlarni hisoblab, natijani lug‘at (dictionary) 
 # shaklida qaytaradi.
+from collections import Counter
+
+numbers = [1,2,3,4,5,4,3,4,5,6,33,6]
+counter = Counter(numbers)
+print(counter)
+
+text = "mississippi"
+counter = Counter(text)
+print(counter)
+
+counter = Counter("banana")
+print(counter.most_common(2))  # Eng ko‘p uchragan 2 ta element
+
+counter = Counter({'a': 4, 'b': 2})
+counter.subtract("ab")  # "a" va "b" bittadan kamayadi
+print(counter)
+
+c1 = Counter("apple")
+c2 = Counter("pear")
+print(c1 - c2)
