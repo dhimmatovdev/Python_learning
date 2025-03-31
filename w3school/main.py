57e0d64cba01c04761d26905778746192c180307
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-03-31 19:35:41 +0500

    31.03

--- w3school/main.py
+++ w3school/main.py
@@ -1,8 +1,18 @@
 # x, y, z = "Tuz"
 
-fruits = ["olma", "anor", "baliq"]
-x, y, z = fruits
-print(x)
-print(y)
-print(z)
+# fruits = ["olma", "anor", "baliq"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
 
+# Global variables 
+
+x = "test"
+
+def myfunc():
+    x = "ok"
+    print("Python is " + x)
+
+myfunc()
+print("Python is " + x)
