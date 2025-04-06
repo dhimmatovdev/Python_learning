c4c44c0bd8fcf8f499343299286fa6cb09e11672
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-04-06 14:06:39 +0500

    06.04

--- w3school/main.py
+++ w3school/main.py
@@ -29,3 +29,30 @@
 
 # print(juft([1,2,4,5,6,9]))
 
+# 2 masala
+
+# Masala: Foydalanuvchidan ismi va yoshi kiritiladigan funksiya yozing.
+# Agar foydalanuvchi yoshi 18 dan kichik bo‘lsa, 
+# unga "Salom, yosh dasturchi!" deb qaytarilsin. 
+# Aks holda "Salom, [ism]!" yozilsin.
+
+# def solom():
+#     yosh = int(input("Yoshingizni kiriting: "))
+#     ism = str(input("Ismingizni kiriting: "))
+#     if yosh < 18:
+#         print("Salom yosh dasturchi ")
+#     else:
+#         print(f"Salom{ism}")
+# solom()
+
+# Optimal yechim
+
+def salom_ber(ism: str, yosh: int):
+    if yosh < 18:
+        return "Salom yosh dasturchi"
+    return f"Salom {ism}"
+if __name__ == "__main__":
+    ism = input("Ismingizni kiriting: ")
+    yosh = int(input("Yoshingizni kiriting: "))
+
+print(salom_ber(ism, yosh))
