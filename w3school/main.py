8cd8f89f94ed6dabc99618b6b310bf7577a56c19
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-04-06 13:52:44 +0500

    06.04

--- w3school/main.py
+++ w3school/main.py
@@ -9,11 +9,23 @@
 # Global variables 
 
 
-x = "test"
+# x = "test"
 
-def myfunc():
-    x = "ok"
-    print("Python is " + x)
+# def myfunc():
+#     x = "ok"
+#     print("Python is " + x)
 
-myfunc()
-print("Python is " + x)
+# myfunc()
+# print("Python is " + x)
+
+# roy =  [1,3,4,8]
+# for i in range(len(roy)):
+#     if roy[i] % 2 == 0:
+#         print("Juft")
+#     else:
+#         print("Toq")    
+# def juft(a):
+#     return sum(1 for i in a if i % 2 ==0)
+
+# print(juft([1,2,4,5,6,9]))
+
